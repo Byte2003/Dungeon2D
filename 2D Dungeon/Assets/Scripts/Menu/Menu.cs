52dd87b6e29c8562dc78bfff81743149cb3d0ce6
triule <trieu0903357103@gmail.com>
2024-11-05 17:56:24 +0700

    Update-Menu-Done

--- 2D Dungeon/Assets/Scripts/Menu/Menu.cs
+++ 2D Dungeon/Assets/Scripts/Menu/Menu.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void OnPlayButton()
-    {
-		if (GameManagerScript.instance != null)
-		{
-			GameManagerScript.instance.Restart();
-		}
-		SceneManager.LoadSceneAsync(1);
-    }
+  //  public void OnPlayButton()
+  //  {
+		//if (GameManagerScript.instance != null)
+		//{
+		//	GameManagerScript.instance.Restart();
+		//}
+		//SceneManager.LoadSceneAsync(1);
+  //  }
 
     public void OnQuitButton()
     {
